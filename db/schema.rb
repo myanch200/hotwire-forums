b8ab89fe426b7d390a0202b615c207c60c3f20da
Martin Yanchev <myanch200@gmail.com>
2023-06-11 18:26:36 +0100

    Adding discussions

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_11_165020) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_172140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "name"
+    t.boolean "pinned", default: false
+    t.boolean "closed", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_discussions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_11_165020) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "discussions", "users"
 end
