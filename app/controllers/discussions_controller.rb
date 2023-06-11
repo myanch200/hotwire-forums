b4bdb5c06107e2f50a2ba52c10e38fc2cf206ba3
Martin Yanchev <myanch200@gmail.com>
2023-06-11 18:53:59 +0100

    adding discussions new and create

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -4,4 +4,27 @@ class DiscussionsController < ApplicationController
   def index
     @discussions = Discussion.all
   end
+
+  def new
+    @discussion = Discussion.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+
+    respond_to do |f|
+      if @discussion.save
+        f.html { redirect_to discussions_path, notice: 'Discussion created successfully.' }
+      else
+        f.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:name, :pinned, :closed)
+  end
 end
