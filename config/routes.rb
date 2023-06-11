af75d3a9f5c4afef1ac3eeecf01a5977f57fba4f
Martin Yanchev <myanch200@gmail.com>
2023-06-11 18:41:43 +0100

    listing discussions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'main#index'
   devise_for :users
+
+  resources :discussions, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
